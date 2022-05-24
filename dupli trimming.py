e4310d390236782801c1d3dda5bd575744e375f1
gbnewb <88551948+gbnewb@users.noreply.github.com>
2022-05-24 19:27:51 +0800

    Update dupli trimming.py

--- dupli trimming.py
+++ dupli trimming.py
@@ -1,7 +1,23 @@
 mdict={}#倉頡碼Dict
 qrdict={}#倉頡碼Dict,value對應qwerty
-qkeytom={}#qwerty to congkit
-with open('tshuocii qwerty.txt',encoding='utf-8') as qwertyCT: #Define a dictionary that maps qwerty keys to congkit mo.
+qrdict2={}#collecting duplicates
+qkeytom={}#qwerty to tshuocii
+'''
+take a entry in the qrdict and variations as arguments
+'''
+def has_varn(entry,*varns):
+    varncount={}
+    for i in range(len(varns)):
+        for ckm in entry:
+            if varns[i] in ckm:
+                varncount[varns[i]]=varncount.get(varns[i],0)+1
+    if len(varncount)==len(varns):
+        return True
+    return False
+def has_varn_print(entry,file,*varns):
+    if has_varn(entry,*varns):
+        print(entry,file=file) 
+with open('tshuocii qwerty.txt',encoding='utf-8') as qwertyCT: #Define a dictionary that maps qwerty keys to tshuocii mo.
     for line in qwertyCT:
         line=line.strip('')
         (qkey, tshcm)=line.split()
@@ -23,7 +39,14 @@ with open('Cangjie5_TC copy 2.txt',encoding='utf-8') as cangjieCT:
     if inp == '0':
         break
     print(f'「{inp}」五代倉頡碼: {mdict[inp]} len:{len(mdict[inp])}')'''
-with open('duplicates4.txt','w',encoding='utf-8') as fow:
+with open('variation dupli.txt','w',encoding='utf-8') as fow:
     for key in qrdict:
         if len(qrdict[key])>1:
-            print(key,qrdict[key],file=fow)
+            if has_varn(qrdict[key],'pi','pm'):
+                print('勺',key,qrdict[key],file=fow)
+            if has_varn(qrdict[key],'kni','knk'):
+                print('丸',key,qrdict[key],file=fow)
+            if has_varn(qrdict[key],'hs','is'):
+                print('戶',key,qrdict[key],file=fow)
+            if has_varn(qrdict[key],'he','me'):
+                print('反',key,qrdict[key],file=fow)
